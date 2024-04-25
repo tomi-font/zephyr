1ffffb384bdc00ade0d804e4f577402e596820f8
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2024-04-25 16:16:44 +0300

    mgmt: mcumgr: replace Tinycrypt by PSA
    As part of ongoing work to move away from TinyCrypt and towards PSA
    (#43712), make fs_mgmt use either PSA (when available) or MbedTLS
    (as a fallback) for SHA-256.
    
    For now this is guarded by CONFIG_BUILD_WITH_TFM.
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

--- subsys/mgmt/mcumgr/grp/fs_mgmt/src/fs_mgmt_hash_checksum_sha256.c
+++ subsys/mgmt/mcumgr/grp/fs_mgmt/src/fs_mgmt_hash_checksum_sha256.c
@@ -13,90 +13,48 @@
 #include <mgmt/mcumgr/grp/fs_mgmt/fs_mgmt_config.h>
 #include <mgmt/mcumgr/grp/fs_mgmt/fs_mgmt_hash_checksum_sha256.h>
 
-#if defined(CONFIG_TINYCRYPT_SHA256)
-#include <tinycrypt/constants.h>
-#include <tinycrypt/sha256.h>
+#if defined(CONFIG_BUILD_WITH_TFM)
+#define PSA_IMPLEMENTATION
+#endif
+
+#if defined(PSA_IMPLEMENTATION)
+#include <psa/crypto.h>
+#define SUCCESS_VALUE PSA_SUCCESS
 #else
-#include <mbedtls/md.h>
 #include <mbedtls/sha256.h>
+#define SUCCESS_VALUE 0
 #endif
 
 #define SHA256_DIGEST_SIZE 32
 
-#if defined(CONFIG_TINYCRYPT_SHA256)
-/* Tinycrypt SHA256 implementation */
 static int fs_mgmt_hash_checksum_sha256(struct fs_file_t *file, uint8_t *output,
 					size_t *out_len, size_t len)
 {
-	int rc = 0;
+	int rc = MGMT_ERR_EUNKNOWN;
+	int op_ret;
 	ssize_t bytes_read = 0;
 	size_t read_size = CONFIG_MCUMGR_GRP_FS_CHECKSUM_HASH_CHUNK_SIZE;
 	uint8_t buffer[CONFIG_MCUMGR_GRP_FS_CHECKSUM_HASH_CHUNK_SIZE];
-	struct tc_sha256_state_struct sha;
-
-	/* Clear variables prior to calculation */
-	*out_len = 0;
-	memset(output, 0, SHA256_DIGEST_SIZE);
-
-	if (tc_sha256_init(&sha) != TC_CRYPTO_SUCCESS) {
-		return MGMT_ERR_EUNKNOWN;
-	}
-
-	/* Read all data from file and add to SHA256 hash calculation */
-	do {
-		if ((read_size + *out_len) >= len) {
-			/* Limit read size to size of requested data */
-			read_size = len - *out_len;
-		}
-
-		bytes_read = fs_read(file, buffer, read_size);
-
-		if (bytes_read < 0) {
-			/* Failed to read file data, pass generic unknown error back */
-			return MGMT_ERR_EUNKNOWN;
-		} else if (bytes_read > 0) {
-			if (tc_sha256_update(&sha, buffer, bytes_read) != TC_CRYPTO_SUCCESS) {
-				return MGMT_ERR_EUNKNOWN;
-			}
-
-			*out_len += bytes_read;
-		}
-	} while (bytes_read > 0 && *out_len < len);
-
-	/* Finalise SHA256 hash calculation and store output in provided output buffer */
-	if (tc_sha256_final(output, &sha) != TC_CRYPTO_SUCCESS) {
-		rc = MGMT_ERR_EUNKNOWN;
-	}
-
-	return rc;
-}
+#if defined(PSA_IMPLEMENTATION)
+	psa_hash_operation_t psa_hash_ctx = psa_hash_operation_init();
 #else
-/* mbedtls SHA256 implementation */
-static int fs_mgmt_hash_checksum_sha256(struct fs_file_t *file, uint8_t *output,
-					size_t *out_len, size_t len)
-{
-	int rc = 0;
-	ssize_t bytes_read = 0;
-	size_t read_size = CONFIG_MCUMGR_GRP_FS_CHECKSUM_HASH_CHUNK_SIZE;
-	uint8_t buffer[CONFIG_MCUMGR_GRP_FS_CHECKSUM_HASH_CHUNK_SIZE];
-	mbedtls_md_context_t mbed_hash_ctx;
-	const mbedtls_md_info_t *mbed_hash_info;
+	mbedtls_sha256_context mbed_hash_ctx;
+#endif
 
 	/* Clear variables prior to calculation */
 	*out_len = 0;
 	memset(output, 0, SHA256_DIGEST_SIZE);
 
-	mbed_hash_info = mbedtls_md_info_from_type(MBEDTLS_MD_SHA256);
-	mbedtls_md_init(&mbed_hash_ctx);
-
-	if (mbedtls_md_setup(&mbed_hash_ctx, mbed_hash_info, 0) != 0) {
+#if defined(PSA_IMPLEMENTATION)
+	if (psa_hash_setup(&psa_hash_ctx, PSA_ALG_SHA_256) != PSA_SUCCESS) {
 		return MGMT_ERR_EUNKNOWN;
 	}
-
-	if (mbedtls_md_starts(&mbed_hash_ctx)) {
-		rc = MGMT_ERR_EUNKNOWN;
-		goto error;
+#else
+	mbedtls_sha256_init(&mbed_hash_ctx);
+	if (mbedtls_sha256_starts(&mbed_hash_ctx, false) != 0) {
+		goto teardown;
 	}
+#endif
 
 	/* Read all data from file and add to SHA256 hash calculation */
 	do {
@@ -108,13 +66,16 @@ static int fs_mgmt_hash_checksum_sha256(struct fs_file_t *file, uint8_t *output,
 		bytes_read = fs_read(file, buffer, read_size);
 
 		if (bytes_read < 0) {
-			/* Failed to read file data, pass generic unknown error back */
-			rc = MGMT_ERR_EUNKNOWN;
-			goto error;
+			/* Failed to read file data */
+			goto teardown;
 		} else if (bytes_read > 0) {
-			if (mbedtls_md_update(&mbed_hash_ctx, buffer, bytes_read) != 0) {
-				rc = MGMT_ERR_EUNKNOWN;
-				goto error;
+#if defined(PSA_IMPLEMENTATION)
+			op_ret = psa_hash_update(&psa_hash_ctx, buffer, bytes_read);
+#else
+			op_ret = mbedtls_sha256_update(&mbed_hash_ctx, buffer, bytes_read);
+#endif
+			if (op_ret != SUCCESS_VALUE) {
+				goto teardown;
 			}
 
 			*out_len += bytes_read;
@@ -122,16 +83,24 @@ static int fs_mgmt_hash_checksum_sha256(struct fs_file_t *file, uint8_t *output,
 	} while (bytes_read > 0 && *out_len < len);
 
 	/* Finalise SHA256 hash calculation and store output in provided output buffer */
-	if (mbedtls_md_finish(&mbed_hash_ctx, output) != 0) {
-		rc = MGMT_ERR_EUNKNOWN;
+#if defined(PSA_IMPLEMENTATION)
+	op_ret = psa_hash_finish(&psa_hash_ctx, output, SHA256_DIGEST_SIZE, &read_size);
+#else
+	op_ret = mbedtls_sha256_finish(&mbed_hash_ctx, output);
+#endif
+	if (op_ret == SUCCESS_VALUE) {
+		rc = 0;
 	}
 
-error:
-	mbedtls_md_free(&mbed_hash_ctx);
+teardown:
+#if defined(PSA_IMPLEMENTATION)
+	psa_hash_abort(&psa_hash_ctx);
+#else
+	mbedtls_sha256_free(&mbed_hash_ctx);
+#endif
 
 	return rc;
 }
-#endif
 
 static struct fs_mgmt_hash_checksum_group sha256 = {
 	.group_name = "sha256",
